9df31a3f02a7abf132070c348d78181d11cabff6
parkdonghoon <daegumian91@gmail.com>
2023-07-12 17:50:57 +0900

    230712

--- Self/src/aProgrammers/Solution.java
+++ Self/src/aProgrammers/Solution.java
@@ -1,42 +1,56 @@
 package aProgrammers;
-
-import java.lang.reflect.Array;
-
 /*
-특정한 문자를 대문자로 바꾸기
+0 떼기
 문제 설명
-영소문자로 이루어진 문자열 my_string과 영소문자 1글자로 이루어진 문자열 alp가 매개변수로 주어질 때, my_string에서 alp에 해당하는 모든 글자를 대문자로 바꾼 문자열을 return 하는 solution 함수를 작성해 주세요.
+정수로 이루어진 문자열 n_str이 주어질 때, n_str의 가장 왼쪽에 처음으로 등장하는 0들을 뗀 문자열을 return하도록 solution 함수를 완성해주세요.
 
 제한사항
-1 ≤ my_string의 길이 ≤ 1,000
+2 ≤ n_str ≤ 10
+n_str이 "0"으로만 이루어진 경우는 없습니다.
 입출력 예
-my_string	alp	result
-"programmers"	"p"	"Programmers"
-"lowercase"	"x"	"lowercase"
+n_str	result
+"0010"	"10"
+"854020"	"854020"
+
  */
 import java.util.*;
 class Solution {
-	public String solution(String my_string, String alp) {
+    public String solution(String n_str) {
         String answer = "";
+        List<String> list = new ArrayList<>();
+        
+        for (int i = 0; i < n_str.length(); i++) {
+			if(n_str.charAt(i) != 0) {
+				break;
+			}else {
+			}
+		}
+        
         return answer;
 	}
+
+
 
 
 	public static void main(String[] args) {
 
 		Solution s = new Solution();
-		//		String[] my_string = {"progressive", "hamburger", "hammer", "ahocorasick"};
-				String my_string = "programmers";
-				String alp = "p";
-		int[][] queries = {{0, 1}, {1,2}, {2,3}};
-		String[] strArr = {"AAA", "BBB", "CCC", "DDD"};
-		int[] num_list = {2, 3, 4, 5};
-		
-		boolean[] finished = {true, false, true, false};
-		String[] todo_list = {"problemsolving", "practiceguitar", "swim", "studygraph"};
+		String[] strArr = {"a","bc","d","efg","hi"};
+		String n_str = "0010";
+		//String pat = "AABB";
+		//String binomial = "43 + 12";
+		//int[][] queries = {{0, 1}, {1,2}, {2,3}};
+		//boolean[] attendance = {false, true, true, true, true, false, false};
+		//int[] num_str = {3, 7, 2, 5, 4, 6, 1};
+		//int[] arr2 = {55, 12, 65, 36};
+		//int n = 27;
+		double flo = 1.42;
+
+		//boolean[] finished = {true, false, true, false};
+		//String[] todo_list = {"problemsolving", "practiceguitar", "swim", "studygraph"};
 		//		String[] intStrs = {"0123456789","9876543210","9999999999999"};
 
-		System.out.println(s.solution(my_string, alp ));
+		System.out.println(s.solution(n_str));
 
 
 	}
